78d3e80c318bf87afa962936e7438836bbe60412
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2018-11-04 21:47:11 -0200

    Add novas dependencias

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
